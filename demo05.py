5a399d9fb4072feebb0e4469cdd5fe7726143698
yuansen0810 <2291368524@qq.com>
2022-03-24 16:37:53 +0800

    第五次向GitHub提交代码

---
+++ demo05.py
@@ -0,0 +1,52 @@
+# print("hello world!") # 字符串
+# print(2333) # 整数
+# print(2.3333) # 小数
+# print(True) # 布尔值
+# print(()) # 元组
+# print([]) # 数组
+# print({}) # 字典
+# '''
+# 锄禾日当午
+# 汗滴禾下土
+# '''
+# print("哈哈哈",2333,2.333)
+# print("哈哈"+"嘻嘻")
+# print("哈哈哈"*100)
+# print(((1+2)*100-9.9)/2)
+# print(2<3)
+
+
+# name = "张三" # 把张三这个值赋值给了叫做a的这个变量，变量为小写字母。
+# print(name)
+# a = input("请输入：")
+# print("input获取的内容：",a)
+# a = input("请输入：")
+# b = input("请输入：")
+# print("input获取的内容：",a+b)
+
+# 数据格式的转换
+
+# print(type(2333))
+# print(type(""))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+# a =int ("2333")
+# print(type(a))
+# a = str()
+# print(type(a))
+# a = float(input("请输入："))
+# b = float(input("请输入："))
+# print("input获取的内容：",a+b)
+# a = int("哈哈哈")
+# print(type(a))
+a = "yegfhjfhggooijiut7rut87tgity"
+print(len(a))
+a = input("请输入：")
+b = input("请输入：")
+print("两端字符串的长度是：",len(a)+len(b))
+
+
+
